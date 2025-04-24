7713e8aa23de277e5366af1e1a0208f328a120b9
Diba Jafarnejad <91078681+diba-jafarnejad@users.noreply.github.com>
2025-04-23 20:40:53 -0400

    Delete app/skincare_app.py

--- app/skincare_app.py
+++
@@ -1,7 +0,0 @@
-from utils.data_loaders import load_sephora_data, load_inci_mapping
-
-# Load data for recommendation logic
-df = load_sephora_data()
-inci_df = load_inci_mapping()
-
-# Use this data for filtering by skin concern, displaying product info, etc.
